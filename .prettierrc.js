02afda38f2f122beec546f38b6eba9391d247aad
leihan <han.lei@mail.hypers.com>
2020-05-13 11:58:21 +0800

    feat: 🎸 新增支持prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,14 @@
 module.exports = {
   printWidth: 100,
   tabWidth: 2,
-  singleQuote: true
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.pug',
+      options: {
+        parser: 'pug',
+        singleQuote: true,
+      },
+    },
+  ],
 };
